f1e5a3ac1685c32e7f27b5402dbe6c9ed2855bbd
Austin Hill <austin.hill42@gmail.com>
2019-08-25 18:36:11 -0500

    AddItem is completely finished!!

--- MEL-TEX Inventory Program/MainPage.xaml.cs
+++ MEL-TEX Inventory Program/MainPage.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MELTEX
 {
@@ -33,6 +21,9 @@ namespace MELTEX
                 {
                     case "Inventory Report":
                         MainWindow.GetWindow(this).Content = new InventoryReport(this);
+                        break;
+                    case "Add Inventory Item":
+                        MainWindow.GetWindow(this).Content = new AddItem(this);
                         break;
                     default:
                         break;
