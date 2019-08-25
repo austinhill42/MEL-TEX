8d0388fc9ed8d7a9a005307d2dc0609efd229102
Austin Hill <austin.hill42@gmail.com>
2019-08-24 19:16:03 -0500

    Add project files.

---
+++ MEL-TEX Inventory Program/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MELTEX
+{
+    /// <summary>
+    /// Interaction logic for MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Inventory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((sender as ComboBox).SelectedValue != null)
+            {
+                switch ((sender as ComboBox).SelectedValue.ToString())
+                {
+                    case "Inventory Report":
+                        MainWindow.GetWindow(this).Content = new InventoryReport(this);
+                        break;
+                    default:
+                        break;
+                } 
+            }
+        }
+
+        private void Main_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindow.GetWindow(this).Title = "Main Window";
+
+            CB_Inventory.SelectedIndex = -1;
+        }
+    }
+}
---
+++ MEL-TEX Inventory Program/NullToVisibilityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MELTEX
+{
+    // VERY IMPORTANT DO NOT DELETE
+    // This is used to display the textblock "Inventory"/"Sales"/etc. when the drop down hasn't been selected
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
