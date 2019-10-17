7c0be79c9159a374497cef52d552f924c84a663c
Austin Hill <austin.hill42@gmail.com>
2019-10-17 06:49:09 -0500

    finished customer_vendor report finished edit customer_vendor

---
+++ MEL-TEX Inventory Program/Customer_VendorReport.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MELTEX
+{
+    /// <summary>
+    /// Interaction logic for Customer_VendorReport.xaml
+    /// </summary>
+    public partial class Customer_VendorReport : Page
+    {
+        public Customer_VendorReport()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustomerDataGridRow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void VendorDataGridRow_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TB_SearchCustomer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TB_SearchVendor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BTN_ClearSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_AddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_EditCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_AddVendor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_EditVendor_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_AddCustomerContact_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_EditCustomerContact_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_AddVendorContact_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_EditVendorContact_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTN_Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
