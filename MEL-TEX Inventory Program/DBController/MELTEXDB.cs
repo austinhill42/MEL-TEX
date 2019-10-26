a60527a6d0de3cb554fad4c3085fa06f3360f387
Austin Hill <austin.hill42@gmail.com>
2019-10-26 09:35:17 -0500

    Created classes for all database tables/binding

--- MEL-TEX Inventory Program/DBController/MELTEXDB.cs
+++ MEL-TEX Inventory Program/DBController/MELTEXDB.cs
@@ -18,6 +18,56 @@ namespace MELTEX.Database
 
         #region Classes
 
+        internal class Groups : MELTEXDB
+        {
+            #region Fields
+
+            private string _Group;
+            private string _Multiplier;
+
+            #endregion Fields
+
+            #region Properties
+
+            public string Group { get => _Group; set { _Group = value; OnPropertyChanged("Group"); } }
+            public string Multiplier { get => _Multiplier; set { _Multiplier = value; OnPropertyChanged("Multiplier"); } }
+
+            #endregion Properties
+        }
+
+        internal class Inventory : MELTEXDB
+        {
+            #region Fields
+
+            private string _Actual_Cost;
+            private string _Barcode_No;
+            private string _BIN;
+            private string _Cost;
+            private string _Inventory_Item;
+            private string _Notes;
+            private string _PO;
+            private string _Quantity;
+            private string _QuantityAvail;
+            private string _Warehouse;
+
+            #endregion Fields
+
+            #region Properties
+
+            public string Actual_Cost { get => _Actual_Cost; set { _Actual_Cost = value; OnPropertyChanged("Actual_Cost"); } }
+            public string Barcode_No { get => _Barcode_No; set { _Barcode_No = value; OnPropertyChanged("Barcode_No"); } }
+            public string BIN { get => _BIN; set { _BIN = value; OnPropertyChanged("BIN"); } }
+            public string Cost { get => _Cost; set { _Cost = value; OnPropertyChanged("Cost"); } }
+            public string Inventory_Item { get => _Inventory_Item; set { _Inventory_Item = value; OnPropertyChanged("Inventory_Item"); } }
+            public string Notes { get => _Notes; set { _Notes = value; OnPropertyChanged("Notes"); } }
+            public string PO { get => _PO; set { _PO = value; OnPropertyChanged("PO"); } }
+            public string Quantity { get => _Quantity; set { _Quantity = value; OnPropertyChanged("Quantity"); } }
+            public string QuantityAvail { get => _QuantityAvail; set { _QuantityAvail = value; OnPropertyChanged("QuantityAvail"); } }
+            public string Warehouse { get => _Warehouse; set { _Warehouse = value; OnPropertyChanged("Warehouse"); } }
+
+            #endregion Properties
+        }
+
         internal class Items : MELTEXDB
         {
             #region Fields
---
+++ MEL-TEX Inventory Program/MEL-TEXAccountingDB.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel;
+
+namespace MELTEX.Database
+{
+    internal class MEL_TEXAccountingDB : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion Events
+
+        #region Methods
+
+        private void OnPropertyChanged(string property) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+
+        #endregion Methods
+
+        #region Classes
+
+        internal class Invoice : MEL_TEXAccountingDB
+        {
+            #region Fields
+
+            private string _AccountNo;
+            private string _Acknowledged;
+            private string _Bank;
+            private string _CheckNo;
+            private string _Confirmation;
+            private string _DateDue;
+            private string _DateReceived;
+            private string _InvoiceNo;
+            private string _Name;
+            private string _Notes;
+            private string _Paid;
+
+            #endregion Fields
+
+            #region Properties
+
+            public string AccountNo { get => _AccountNo; set { _AccountNo = value; OnPropertyChanged("AccountNo"); } }
+            public string Acknowledged { get => _Acknowledged; set { _Acknowledged = value; OnPropertyChanged("Acknowledged"); } }
+            public string Bank { get => _Bank; set { _Bank = value; OnPropertyChanged("Bank"); } }
+            public string CheckNo { get => _CheckNo; set { _CheckNo = value; OnPropertyChanged("CheckNo"); } }
+            public string Confirmation { get => _Confirmation; set { _Confirmation = value; OnPropertyChanged("Confirmation"); } }
+            public string DateDue { get => _DateDue; set { _DateDue = value; OnPropertyChanged("DateDue"); } }
+            public string DateReceived { get => _DateReceived; set { _DateReceived = value; OnPropertyChanged("DateReceived"); } }
+            public string InvoiceNo { get => _InvoiceNo; set { _InvoiceNo = value; OnPropertyChanged("InvoiceNo"); } }
+            public string Name { get => _Name; set { _Name = value; OnPropertyChanged("Name"); } }
+            public string Notes { get => _Notes; set { _Notes = value; OnPropertyChanged("Notes"); } }
+            public string Paid { get => _Paid; set { _Paid = value; OnPropertyChanged("Paid"); } }
+
+            #endregion Properties
+        }
+
+        #endregion Classes
+    }
+}
