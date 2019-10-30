2aad29dab26d380f16f888e3976ddade575c4832
Austin Hill <austin.hill42@gmail.com>
2019-10-30 09:12:48 -0500

    work order fix -- finished

--- MEL-TEX Inventory Program/GenerateWorkOrder.xaml.cs
+++ MEL-TEX Inventory Program/GenerateWorkOrder.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MELTEX
 {
@@ -25,6 +18,7 @@ namespace MELTEX
         private Page PreviousPage;
         internal Data data;
         private DataTable items;
+        private List<WorkOrderItem> LineItems = new List<WorkOrderItem>();
 
         public GenerateWorkOrder(Page prev, Data data)
         {
@@ -53,8 +47,26 @@ namespace MELTEX
 
             L_Num.Content = $"Sales Order: {data.number}";
 
-            items = data.table.AsDataView().ToTable(true, "Item ID", "Barcode", "Description");
+            items = data.table.AsDataView().ToTable(false, "Item ID", "Barcode", "Description");
 
+            //items.AsEnumerable().ToList().ForEach(r => PopulateItems(r));
+
+            foreach (DataRow row in items.Rows)
+                PopulateItems(row);
+
+        }
+
+        private void PopulateItems(DataRow r)
+        {
+            WorkOrderItem item = new WorkOrderItem(r["Item ID"].ToString(), r["Barcode"].ToString(), r["Description"].ToString());
+            RowDefinition row = new RowDefinition();
+            row.Height = new GridLength(200);
+            Grid.RowDefinitions.Add(row);
+            Grid.Children.Add(item);
+            Grid.SetRow(item, Grid.RowDefinitions.Count - 1);
+
+            LineItems.Add(item);
+            
         }
 
         #region Structs
@@ -120,9 +132,68 @@ namespace MELTEX
                 cb.SelectedIndex = -1;
         }
 
-        private void BTN_Save_Click(object sender, RoutedEventArgs e)
+        private void BTN_Print_Click(object sender, RoutedEventArgs e)
         {
+            BinaryFormatter formatter = new BinaryFormatter();
+            string workOrderLoc = App.loc + "Data\\Work Orders\\";
 
+            Directory.CreateDirectory(workOrderLoc);
+
+            FileStream stream = new FileStream(workOrderLoc + data.number, FileMode.Create, FileAccess.Write);
+
+            formatter.Serialize(stream, data);
+            stream.Close();
+
+            StreamWriter write = new StreamWriter(workOrderLoc + data.number + ".csv");
+
+            write.WriteLine($"{DateTime.Now.Date.ToShortDateString()};Sales Order: {data.number};Quote: {data.quoteNum};{data.buyer.Replace("\n", "\\n").Replace("\r", "\\n").Replace(Environment.NewLine, "\\n")};" +
+                $"{data.selectedShipTo.Replace("\n", "\\n").Replace("\r", "\\n").Replace(Environment.NewLine, "\\n")};{data.billTo.Replace("\n", "\\n").Replace("\r", "\\n").Replace(Environment.NewLine, "\\n")};" +
+                $"{data.shipVia};{data.terms};{data.fob};{data.freightTerms};{data.repNum};{data.repName}");
+
+            foreach (WorkOrderItem item in LineItems)
+            {
+                string id = item.ItemID + "<br/>";
+                string barcode = item.Barcode + "<br/>";
+                string desc = item.Description + "<br/>";
+                string notes = item.Notes.Replace(Environment.NewLine, "<br/>");
+
+                write.Write($";{id}{barcode}{desc}<br/><br/>{notes}<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>");
+
+            }
+
+            write.Close();
+
+            generatePDF(true);
+        }
+
+        private void generatePDF(bool print)
+        {
+            string loc = Path.Combine(AppDomain.CurrentDomain.BaseDirectory);
+            string pyexe = $"{loc}Python37_64\\python.exe";
+            string pyscript = $"{loc}Work Order PDF.py";
+            string loadfile = $"{loc}Data\\Work Orders\\{data.number}.csv";
+            string savefile = $"{loc}Work Orders\\{data.number}.pdf";
+
+            Directory.CreateDirectory($"{loc}Data\\Work Orders");
+            Directory.CreateDirectory($"{loc}Work Orders");
+
+            ProcessStartInfo start = new ProcessStartInfo
+            {
+                FileName = pyexe,
+                Arguments = string.Format("\"{0}\" \"{1}\" \"{2}\" {3}", pyscript, loadfile, savefile, print ? "print" : ""),
+                UseShellExecute = false,
+                RedirectStandardOutput = true
+            };
+
+            using (Process process = Process.Start(start))
+            {
+                process.WaitForExit();
+
+                if (process.ExitCode != 0)
+                    MessageBox.Show("Something went wrong!!");
+                else if (!print)
+                    MessageBox.Show("Work Order Saved");
+            }
         }
     }
 }
---
+++ MEL-TEX Inventory Program/WorkOrderItem.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MELTEX
+{
+    /// <summary>
+    /// Interaction logic for WorkOrderItem.xaml
+    /// </summary>
+    public partial class WorkOrderItem : UserControl
+    {
+        public string ItemID { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+
+        public WorkOrderItem(string itemID, string barcode, string description)
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            ItemID = $"Item ID: {itemID}";
+            Barcode = $"Barcode: {barcode}";
+            Description = $"Description: {description}";
+        }
+    }
+}
